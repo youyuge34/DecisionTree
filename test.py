95a791aa2cc7fe3c0042350987e0aec1203f49d9
Andy <1197993367@qq.com>
2017-09-02 15:32:35 +0800

    3.2 matplotlib绘制决策树完毕，测试ok

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 # encoding: utf-8
 
 import trees
+import treePlotter
 
 def run():
     dataSet,labels = trees.createDataSet()
-    print dataSet
-    print trees.calcShannonEnt(dataSet)
-    print trees.chooseBestFeatureToSplit(dataSet)
-    print trees.createTree(dataSet, labels)
+    # print dataSet
+    # print trees.calcShannonEnt(dataSet)
+    # print trees.chooseBestFeatureToSplit(dataSet)
+    # print trees.createTree(dataSet, labels)
+    myTree = treePlotter.retrieveTree(1)
+    treePlotter.createPlot(myTree)
 
 if __name__ == '__main__':
     run()
